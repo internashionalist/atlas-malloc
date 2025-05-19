33fb8cd89667f9ce6499c5c8c9c87148e3dfa727
Nash Thames <taylor.thames@atlasschool.com>
2025-05-19 14:42:29 +0000

    used struct

--- free.c
+++ free.c
@@ -4,24 +4,22 @@
  * _free - frees a memory space
  * @ptr: pointer to the memory space to be freed
  *
- * This function frees a memory space that was previously allocated
- * using the _malloc function. It checks if the pointer is valid
- * and if it is at the current break. If so, it releases the memory
- * back to the system using sbrk.
+ * T
  *
  * Return: void
  */
 void _free(void *ptr)
 {
-	size_t current_break = (size_t)sbrk(0);
-	size_t header_size = alignof(max_align_t);
+	block_header_t *blk = (block_header_t *)ptr - 1; /* get the header */
 
-	/*
-	* check if valid memory space?
-	* check if at current break
-	* something
-	*/
+	blk->free = 1; /* mark the block as free */
 
-	if (current_break == (size_t)ptr)
-		sbrk(-1 * header_size);
+	/* merge with previous block if free */
+	if (blk->next && blk->next->free)
+	{
+		/* merge with next block */
+		blk->size += sizeof(block_header_t) + blk->next->size;
+		/* advance the pointer */
+		blk->next = blk->next->next;
+	}
 }
