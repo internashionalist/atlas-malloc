b7b6596cb9aa694d8f737aaf94a519b416abad43
Nash Thames <taylor.thames@atlasschool.com>
2025-05-19 04:50:36 +0000

    betty

--- free.c
+++ free.c
@@ -2,15 +2,25 @@
 
 /**
  * _free - frees a memory space
+ * @ptr: pointer to the memory space to be freed
+ *
+ * This function frees a memory space that was previously allocated
+ * using the _malloc function. It checks if the pointer is valid
+ * and if it is at the current break. If so, it releases the memory
+ * back to the system using sbrk.
+ *
+ * Return: void
  */
 void _free(void *ptr)
 {
 	size_t current_break = (size_t)sbrk(0);
 	size_t header_size = alignof(max_align_t);
 
-	//check if valid memory space?
-	//check if at current break
-	//something
+	/*
+	* check if valid memory space?
+	* check if at current break
+	* something
+	*/
 
 	if (current_break == (size_t)ptr)
 		sbrk(-1 * header_size);
